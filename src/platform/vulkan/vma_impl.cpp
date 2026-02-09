8ce133a5beb5083b04df6c93c658723d4be0c9e2
Christian Howe <cj@cjhowe.us>
2026-02-08 21:31:07 -0500

    Delete everything

--- src/platform/vulkan/vma_impl.cpp
+++
@@ -1,9 +0,0 @@
-// vma_impl.cpp - VulkanMemoryAllocator implementation
-// This file provides the single translation unit for VMA
-
-#define VMA_IMPLEMENTATION
-#define VMA_STATIC_VULKAN_FUNCTIONS 0
-#define VMA_DYNAMIC_VULKAN_FUNCTIONS 1
-
-#include <volk.h>
-#include <vk_mem_alloc.h>
